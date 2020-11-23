3dd492521cbc73212687c40ce891e99f1e64aa3d
amrizaaama <56916812+amrizaaama@users.noreply.github.com>
2020-11-23 21:39:58 +0700

    Initial commit

---
+++ app/src/main/java/org/aplas/databinding/MatchResultBinding.java
@@ -0,0 +1,16 @@
+package org.aplas.databinding;
+
+public class MatchResultBinding extends android.databinding.ViewDataBinding {
+    public final android.widget.ImageView resultGif;
+    public final android.widget.TextView homeTeam;
+    public final android.widget.TextView homeGoals;
+    public final android.widget.TextView awayTeam;
+    public final android.widget.TextView awayGoals;
+
+    private MatchResultBinding binding;
+
+    @Override protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.set
+    }
+}
